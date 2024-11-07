38e09dd91d7b847251636da5f72c2b4756813783
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2024-11-07 11:13:28 +0100

    add POST request unit tests

--- src/test/java/at/technikum/server/util/HttpRequestParserTest.java
+++ src/test/java/at/technikum/server/util/HttpRequestParserTest.java
@@ -12,19 +12,17 @@ class HttpRequestParserTest {
             GET /home HTTP/1.1
             Host: localhost:10001
             Authentication: Bearer example-token
-            
-            """.replace("\n","\r\n");
+            \n""".replace("\n","\r\n");
 
     private final static String HTTP_POST = """
             POST /users HTTP/1.1
             Host: localhost:8080
             Content-Type: application/json
-            Content-Length: 30
+            Content-Length: 31
             
             {
-                "username": "example",
-            }
-            """.replace("\n","\r\n");
+                "username": "example"
+            }""".replace("\n","\r\n");
 
     private final HttpRequestParser requestParser = new HttpRequestParser();
 
@@ -54,5 +52,37 @@ class HttpRequestParserTest {
         Request request = requestParser.parse(HTTP_GET);
 
         assertEquals("Bearer example-token", request.getHeader("Authentication"));
+    }
+
+    @Test
+    public void given_httpPostRequest_then_parseMethod() {
+        Request request = requestParser.parse(HTTP_POST);
+
+        assertEquals(Method.POST, request.getMethod());
+    }
+
+    @Test
+    public void given_httpPostRequest_then_parseContentLength() {
+        Request request = requestParser.parse(HTTP_POST);
+
+        assertEquals("31", request.getHeader("Content-Length"));
+    }
+
+    @Test
+    public void given_httpPostRequest_then_parseContentType() {
+        Request request = requestParser.parse(HTTP_POST);
+
+        assertEquals("application/json", request.getHeader("Content-Type"));
+    }
+
+    @Test
+    public void given_httpPostRequest_then_parseBody() {
+        Request request = requestParser.parse(HTTP_POST);
+
+        String body = """
+                {
+                    "username": "example"
+                }""".replace("\n", "\r\n");
+        assertEquals(body, request.getBody());
     }
 }
