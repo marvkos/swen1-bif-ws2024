5f447fa959654beb336640bcd6007d3ff0c19770
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2024-11-12 17:40:32 +0100

    finsih server skeleton

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -1,10 +1,13 @@
 package at.technikum;
 
+import at.technikum.application.echo.EchoApplication;
+import at.technikum.application.html.SimpleHtmlApplication;
+import at.technikum.server.Server;
 import at.technikum.server.http.Request;
 
 public class Main {
     public static void main(String[] args) {
-        // Start server
-        // server.start()
+        Server server = new Server(new EchoApplication());
+        server.start();
     }
 }
--- src/main/java/at/technikum/server/util/HttpResponseFormatter.java
+++ src/main/java/at/technikum/server/util/HttpResponseFormatter.java
@@ -2,10 +2,37 @@ package at.technikum.server.util;
 
 import at.technikum.server.http.Response;
 
+import java.util.Map;
+
 public class HttpResponseFormatter {
 
     // java http res obj to string
     public String format(Response response) {
-        return null;
+        StringBuilder responseBuilder = new StringBuilder();
+
+        if (null == response.getStatus()) {
+            throw new NoHttpStatusException("Response does not contain a status");
+        }
+
+        responseBuilder.append("HTTP/1.1 ").append(response.getStatus()).append("\r\n");
+
+        if (response.getBody() == null || response.getBody().isEmpty()) {
+            response.setHeader("Content-Length", "0");
+        } else {
+            response.setHeader("Content-Length", "%s".formatted(response.getBody().length()));
+        }
+
+        for (Map.Entry<String, String> header: response.getHeaders().entrySet()) {
+            responseBuilder.append(header.getKey()).append(": ").append(header.getValue()).append("\r\n");
+        }
+
+        if (response.getBody() == null || response.getBody().isEmpty()) {
+            return responseBuilder.toString();
+        }
+
+        responseBuilder.append("\r\n");
+        responseBuilder.append(response.getBody());
+
+        return responseBuilder.toString();
     }
 }
