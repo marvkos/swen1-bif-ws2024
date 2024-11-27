982662a95967ab2476373bc3b14e46d5c4254e4c
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2024-11-27 16:11:15 +0100

    add postgres driver and docker compose file

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -4,7 +4,6 @@ import at.technikum.application.echo.EchoApplication;
 import at.technikum.application.html.SimpleHtmlApplication;
 import at.technikum.application.moodle.Moodle;
 import at.technikum.server.Server;
-import at.technikum.server.http.Request;
 
 public class Main {
     public static void main(String[] args) {
